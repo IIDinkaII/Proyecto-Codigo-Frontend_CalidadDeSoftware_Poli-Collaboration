ff92ffc43ffe12373a3ccb98d3df648f46bffd04
Diana <diana.lopez03@epn.edu.ec>
2021-07-10 20:52:18 -0500

    Preparación del entorno de desarrollo
    Inclusión de Chakra-ui

--- poli-collaboration/pages/login.js
+++ poli-collaboration/pages/login.js
@@ -1,11 +1,32 @@
 import React from 'react'
+import { Button, ButtonGroup } from "@chakra-ui/react"
+import { Input } from "@chakra-ui/react"
+import { Text } from "@chakra-ui/react"
 
 const Login = () => {
     return(
         <div>
-            <p>Login</p>
+            <Text fontSize="50px" color="black" align="center">
+                Login
+            </Text>
+            <div>
+                <Text color="gray.500" isTruncated>
+                    Usuario:
+                </Text>
+                <Input w ="400px" placeholder="Ingresa el usuario" isRequired = "true" />
+                <br></br>
+                <Text color="gray.500" isTruncated>
+                    Contraseña:
+                </Text>
+                <Input w ="400px" placeholder="Ingresa su contraseña" isRequired = "true" />
+            </div>
+
+            <div>
+                <Button colorScheme="blue">Button</Button>
+            </div>
         </div>
     )
 }
 
-export default Login
+
+  export default Login
