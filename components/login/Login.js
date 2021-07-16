462b7b23ab5fd8beb07738e5a659cfdd68af58cc
Alejandro Llanganate <luis.llanganate@epn.edu.ec>
2021-07-16 17:47:04 -0500

    refactoring Login

--- components/login/Login.js
+++ components/login/Login.js
@@ -1,46 +1,47 @@
 import React from 'react';
-import { Button, ButtonGroup, Flex, Icon, InputGroup, InputLeftAddon, Input, Heading, InputRightElement, Checkbox } from '@chakra-ui/react';
-import { MdSettings, MdEmail, MdLock, MdView } from 'react-icons/md';
-import { ViewIcon, ViewOffIcon, WarningIcon, EmailIcon, LockIcon } from '@chakra-ui/icons';
+import { Button, Flex, InputGroup, InputLeftAddon, Input, Heading, InputRightElement, Checkbox, VStack, HStack } from '@chakra-ui/react';
+import { ViewIcon, ViewOffIcon, EmailIcon, LockIcon } from '@chakra-ui/icons';
 
 const Login = () => {
-  const [show, setShow] = React.useState(false);
-  const handleClick = () => setShow(!show);
+  const [showPassword, setShowPassword] = React.useState(false);
+  const handleClick = () => setShowPassword(!showPassword);
   return (
     <Flex direction="column" p={12}>
-      <Flex rounded={3} justifyContent="center" alignItems="center">
-        <Heading size="lg">Inicio de sesión</Heading>
-      </Flex>
-      <InputGroup mt={4}>
-        <InputLeftAddon>
-          <EmailIcon />
-        </InputLeftAddon>
-        <Input type="email" placeholder="Correo electrónico"></Input>
-      </InputGroup>
-      <InputGroup mt={4}>
-        <InputLeftAddon>
-          <LockIcon />
-        </InputLeftAddon>
-        <Input pr="4.5rem" type={show ? 'text' : 'password'} placeholder="Ingrese su contraseña" />
-        <InputRightElement width="4.5rem">
-          <Button h="1.75rem" size="sm" onClick={handleClick}>
-            {show ? <ViewOffIcon /> : <ViewIcon />}
-          </Button>
-        </InputRightElement>
-      </InputGroup>
-      <Flex justifyContent="flex-end">
-        <Checkbox mt={4} mb={4} flex="left" defaultIsChecked>
+      <VStack spacing={5} justify="flex-end">
+        <Flex rounded={3}>
+          <Heading size="lg">Inicio de sesión</Heading>
+        </Flex>
+        {/* Correo electronico */}
+        <InputGroup>
+          <InputLeftAddon>
+            <EmailIcon />
+          </InputLeftAddon>
+          <Input type="email" placeholder="Correo electrónico"></Input>
+        </InputGroup>
+        {/* Password */}
+        <InputGroup>
+          <InputLeftAddon>
+            <LockIcon />
+          </InputLeftAddon>
+          <Input pr="4.5rem" type={showPassword ? 'text' : 'password'} placeholder="Ingrese su contraseña" />
+          <InputRightElement width="4.5rem">
+            <Button h="1.75rem" size="sm" onClick={handleClick}>
+              {showPassword ? <ViewOffIcon /> : <ViewIcon />}
+            </Button>
+          </InputRightElement>
+        </InputGroup>
+      </VStack>
+      {/* Recordar Credenciales */}
+      <HStack mt={5} mb={3} justify="flex-end">
+        <Checkbox flex="left" defaultIsChecked>
           Recordar credenciales
         </Checkbox>
-      </Flex>
-      <Flex direction="row" justifyContent="center">
-        <Button colorScheme="green" m={4}>
-          Registrarse
-        </Button>
-        <Button colorScheme="blue" m={4}>
-          Ingresar
-        </Button>
-      </Flex>
+      </HStack>
+      {/* Botones  */}
+      <HStack mt={5} justify="center" spacing={5}>
+        <Button colorScheme="green">Registrarse</Button>
+        <Button colorScheme="blue">Ingresar</Button>
+      </HStack>
     </Flex>
   );
 };
