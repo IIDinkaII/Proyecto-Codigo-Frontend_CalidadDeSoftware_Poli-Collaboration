506f3631fa995bbcc48a0f645617fbe572aa8c93
Diana <diana.lopez03@epn.edu.ec>
2021-07-29 00:06:03 -0500

    feat(signup,login): create frontend validations

--- components/login/SignUp.js
+++ components/login/SignUp.js
@@ -17,6 +17,8 @@ import {
 import Login from './Login';
 import { ViewIcon, ViewOffIcon } from '@chakra-ui/icons';
 import Terms from '../resources/Terms';
+import { Form, Formik, Field } from 'formik';
+import * as Yup from 'yup';
 
 const SignUp = () => {
   // Contraseña
@@ -25,6 +27,19 @@ const SignUp = () => {
   // Confirmar contraseña
   const [showPassConfirmation, setShowPassConf] = React.useState(false);
   const handleClickConf = () => setShowPassConf(!showPassConfirmation);
+  // Validaciones FrontEnd
+  const SignUpSchema = Yup.object().shape({
+    nombres: Yup.string().min(3, 'Prueba con un nombre más largo.').max(70, 'Tu nombre debe ser más corto.').required('Campo obligatorio'),
+    apellidos: Yup.string().min(2, 'Prueba con un apellido más largo.').max(70, 'Tu nombre debe ser más largo.').required('Campo obligatorio'),
+    correoInstitucional: Yup.string().email('Dirección de correo electrónico no válida').required('Campo obligatorio').regex('^[a-z]{3,15}.[a-z]{3,15}[0-9]{0,2}(@epn.edu.ec)$'),
+    carrera: Yup.string().required(),
+    sexo: Yup.string().required(),
+    fechaNacimiento: Yup.date().required(),
+    password: Yup.string().required('Campo obligatorio'),
+    passwordConfirmation: Yup.string()
+      .oneOf([Yup.ref('password'), null], 'Las contraseñas no coinciden')
+      .required('Campo obligatorio'),
+  });
 
   return (
     <SimpleGrid columns={2} spacingX="40px">
