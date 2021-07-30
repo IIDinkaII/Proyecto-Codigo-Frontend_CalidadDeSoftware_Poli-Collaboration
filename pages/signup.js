90b0513ef4c5361aaab643a59ead61c67cd95de3
Diana <diana.lopez03@epn.edu.ec>
2021-07-29 23:11:33 -0500

    update: add input validation

--- pages/signup.js
+++ pages/signup.js
@@ -31,6 +31,7 @@ const SignUp = () => {
   const [showPassConfirmation, setShowPassConf] = useState(false);
   const handleClickConf = () => setShowPassConf(!showPassConfirmation);
 
+  // Validaciones FrontEnd
   const initialFieldValue = '';
 
   const SignUpSchema = Yup.object().shape({
@@ -39,57 +40,29 @@ const SignUp = () => {
       .min(2, 'Prueba con un apellido más largo.')
       .max(70, 'Tu nombre debe ser más largo.')
       .required('Campo obligatorio'),
-    correoInstitucional: Yup.string().email('Dirección de correo electrónico no válida').required('Campo obligatorio'),
+    correoInstitucional: Yup.string().email('Dirección de correo electrónico no válida').required('Campo obligatorio').matches('^[a-z]{3,15}.[a-z]{3,15}[0-9]{0,2}(@epn.edu.ec)$',"Correo no perteneciente a la EPN"),
     sexo: Yup.string().required('Campo obligatorio'),
-    carrera: Yup.string().required(),
-    fechaNacimiento: Yup.date().required(),
+    carrera: Yup.string().required('Campo obligatorio'),
+    fechaNacimiento: Yup.date().required('Campo obligatorio'),
     password: Yup.string().required('Campo obligatorio'),
-    passwordConfirmation: Yup.string()
+    passwordConfirmation: Yup.string().required('Campo obligatorio')
       .oneOf([Yup.ref('password'), null], 'Las contraseñas no coinciden')
-      .required('Campo obligatorio'),
+      
   });
-
-  // const onSubmit = (e) => {
-  //   e.preventDefault();
-  //   console.log('open', areFieldsValid);
-
-  //   if (areFieldsValid) {
-  //     console.log('shit', areFieldsValid);
-  //     resetHooks();
-  //     axios
-  //       .post(`${environment.api}/usuario`, usuario, {
-  //         headers: {
-  //           'Content-Type': 'application/json',
-  //         },
-  //       })
-  //       .then((res) => {
-  //         alert('Usuario registrado');
-  //       })
-  //       .catch((error) => {
-  //         if (error.response.data.statusCode == 406) {
-  //           setValidEmail(false);
-  //         }
-  //       });
-  //   } else {
-  //     console.log('exit');
-  //     setRegistroForm(false);
-  //   }
-  // };
 
   const formik = useFormik({
     initialValues: {
       nombres: initialFieldValue,
       apellidos: initialFieldValue,
       correoInstitucional: initialFieldValue,
-      carrera: initialFieldValue,
       sexo: initialFieldValue,
+      carrera: initialFieldValue,
       fechaNacimiento: initialFieldValue,
       password: initialFieldValue,
       passwordConfirmation: initialFieldValue,
     },
     validationSchema: SignUpSchema,
     onSubmit: (formData) => {
-      console.log('me aplastaste');
       console.log(formData);
     },
   });
@@ -114,69 +87,69 @@ const SignUp = () => {
               </FormControl>
               <FormControl width="50%" id="apellidoUsuario" isInvalid={formik.errors.apellidos && formik.touched.apellidos}>
                 <Input placeholder="Apellidos del estudiante *" name="apellidos" onChange={formik.handleChange} />
-              </FormControl>
+              </FormControl>         
             </HStack>
-            <div>
-            <Text>{formik.errors.nombres}</Text>
-            <Text>{formik.errors.apellidos}</Text>
 
-            </div>
-
-
+            <HStack px={10}>
+              <Text width="50%" fontSize="xs" color="red.500">{formik.errors.nombres}</Text>
+              <Text width="50%" fontSize="xs" color="red.500">{formik.errors.apellidos}</Text>
+            </HStack>
+            
             {/* Correo */}
-            <FormControl
-              px={10}
-              py={2}
-              mt={2}
-              mb={2}
-              id="correoInstitucional"
-              isInvalid={formik.errors.correoInstitucional && formik.touched.correoInstitucional}
-            >
+            <FormControl px={10} py={2} mt={2} id="correoInstitucional"
+              isInvalid={formik.errors.correoInstitucional && formik.touched.correoInstitucional}>
               <Input
                 placeholder="Correo institucional (Ej.: nombre.apellido@epn.edu.ec)"
                 name="correoInstitucional"
                 onChange={formik.handleChange}
               />
-              <FormErrorMessage>{formik.errors.correoInstitucional}</FormErrorMessage>
+              <Text py="2" fontSize="xs" color="red.500">{formik.errors.correoInstitucional}</Text>
             </FormControl>
+
+            
             {/* Carrera */}
-            <FormControl id="carreraUsuario" px={10} py={2} mt={2} mb={2} isInvalid={formik.errors.carrera && formik.touched.carrera}>
-              <Select id="a" placeholder="Seleccione una carrera" variant="filled" name="carrera" onChange={formik.handleChange}>
-                <option id="b" value="a">
+            <FormControl id="carreraUsuario" px={10} py={2} isInvalid={formik.errors.carrera && formik.touched.carrera}>
+              <Select id="carrera" placeholder="Seleccione una carrera" variant="filled" name="carrera" onChange={formik.handleChange}>
+                <option id="1" value="software">
                   Ingeniería de Software
                 </option>
-                <option id="c" value="b">
+                <option id="2" value="computacion">
                   Ciencias de la Computación
                 </option>
-                <option id="d" value="c">
+                <option id="3" value="sistemas">
                   Ingeniería en Sistemas
                 </option>
               </Select>
-              <FormErrorMessage>{formik.errors.email}</FormErrorMessage>
             </FormControl>
+            
+            <HStack px={10}>
+              <Text width="50%" fontSize="xs" color="red.500">{formik.errors.carrera}</Text>
+            </HStack>
 
             {/* Sexo y fecha de nacimiento */}
-            <HStack px={10} py={2}>
+            <HStack px={10} py={2}  mt={2}>
+
               <FormControl width="50%" id="sexoUsuario" isInvalid={formik.errors.sexo && formik.touched.sexo}>
-                <Select id="a" placeholder="Seleccione su sexo" variant="filled" name="sexo" onChange={formik.handleChange}>
-                  <option id="b" value="">
-                    Hombre
-                  </option>
-                  <option id="c" value="">
-                    Mujer
-                  </option>
+                <Select id="sexo" placeholder="Seleccione su sexo" variant="filled" name="sexo" onChange={formik.handleChange}>
+                  <option id="1" value="hombre">Hombre</option>
+                  <option id="2" value="mujer">Mujer</option>
                 </Select>
-                <FormErrorMessage>{formik.errors.sexo}</FormErrorMessage>
               </FormControl>
+
               <FormControl
                 width="50%"
-                id="fechaNacimientoUsuario"
+                id="fechaNacimiento"
                 isInvalid={formik.errors.fechaNacimiento && formik.touched.fechaNacimiento}
               >
-                <Input type="date" name="fechaNacimientoUsuario" onChange={formik.handleChange} />
-                <FormErrorMessage>{formik.errors.fechaNacimientoUsuario}</FormErrorMessage>
+                <Input type="date" name="fechaNacimiento" onChange={formik.handleChange} />
               </FormControl>
             </HStack>
+
+            <HStack px={10}>
+              <Text width="50%" fontSize="xs" color="red.500">{formik.errors.sexo}</Text>
+              <Text width="50%" fontSize="xs" color="red.500">{formik.errors.fechaNacimiento}</Text>
+            </HStack>
+
             {/* Contraseña */}
             <HStack mt={2} px={10} py={2}>
               <FormControl width="50%" id="passwordUsuario" isInvalid={formik.errors.password && formik.touched.password}>
@@ -184,7 +157,6 @@ const SignUp = () => {
                 <InputRightElement h="100%">
                   <Button onClick={handleClick}>{showPassword ? <ViewOffIcon /> : <ViewIcon />}</Button>
                 </InputRightElement>
-                <FormErrorMessage>{formik.errors.password}</FormErrorMessage>
               </FormControl>
               <FormControl
                 width="50%"
@@ -193,15 +165,21 @@ const SignUp = () => {
               >
                 <Input
                   placeholder="Confirmación de contraseña"
+                  name = "passwordConfirmation"
                   onChange={formik.handleChange}
                   type={showPassConfirmation ? 'text' : 'password'}
                 />
                 <InputRightElement h="100%">
                   <Button onClick={handleClickConf}>{showPassConfirmation ? <ViewOffIcon /> : <ViewIcon />}</Button>
                 </InputRightElement>
-                <FormErrorMessage>{formik.errors.passwordConfirmation}</FormErrorMessage>
               </FormControl>
             </HStack>
+
+            <HStack px={10}>
+              <Text width="50%" fontSize="xs" color="red.500">{formik.errors.password}</Text>
+              <Text width="50%" fontSize="xs" color="red.500">{formik.errors.passwordConfirmation}</Text>
+            </HStack>
+
             {/* Aceptar terminos y condiciones */}
             <FormControl id="confirmacionTerminos" display="flex" justifyContent="center" py={4}>
               <Switch id="emailAlerts" pr={5} />
